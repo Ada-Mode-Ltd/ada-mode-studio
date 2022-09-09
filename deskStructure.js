9105f87cdc0155b1dc242f93a610bf26adf7fb5b
Fershad <itsfish@fershad.com>
2022-09-09 12:40:45 +0800

    update desk structure

--- deskStructure.js
+++ deskStructure.js
@@ -19,55 +19,88 @@ const siteSpecificSchema = (title, site, schemaType, validationField) => {
     .title(title)
     .child(
       S.documentTypeList(schemaType)
-        .title(`${company} ${title}`)
-        .filter('_type == $type && $site in publishTo')
-        .params({type: schemaType, site})
-        .child(documentId =>
-          S.document()
-            .documentId(documentId)
-            .schemaType(schemaType)
-        )
+      .title(`${company} ${title}`)
+      .filter('_type == $type && $site in publishTo')
+      .params({
+        type: schemaType,
+        site
+      })
+      .child(documentId =>
+        S.document()
+        .documentId(documentId)
+        .schemaType(schemaType)
+      )
     )
-        }
+}
 
 
 export default () =>
-  S.list()
-    .title('Base')
-    .items([
+S.list()
+  .title('Base')
+  .items([
+    S.listItem()
+    .title('Ada Mode')
+    .child(
+      S.list()
+      .title('Content')
+      .id('am')
+      .items([
+        siteSpecificSchema('Posts', 'am', 'post', 'publishTo'),
+        siteSpecificSchema('Job listings', 'am', 'job', 'publishTo'),
+        siteSpecificSchema('General page', 'am', 'generalPage', 'publishTo'),
+        // S.documentTypeListItems().filter(
+        //     item => item.getSchemaType().name.startsWith('am-')
+        //   )
+      ])
+    ),
+    S.listItem()
+    .title('Windscope')
+    .child(
+      S.list()
+      .title('Content')
+      .id('ws')
+      .items([
+        siteSpecificSchema('Posts', 'ws', 'post', 'publishTo'),
+        siteSpecificSchema('CTA pages', 'ws', 'ctaPage', 'publishTo'),
+        siteSpecificSchema('General page', 'ws', 'generalPage', 'publishTo'),
+        // S.documentTypeListItems().filter(
+        //     item => item.getSchemaType().name.startsWith('ws-')
+        //   )  
+      ])
+    ),
+    S.divider(),
+    S.listItem()
+    .title('Blog settings')
+    .child(
+      S.list()
+       .title('All blog settings')
+       .items([
       S.listItem()
-        .title('Ada Mode')
-        .child(
-          S.list()
-            .title('Content')
-            .id('am')
-            .items([
-              siteSpecificSchema('Posts', 'am', 'post', 'publishTo'),
-              siteSpecificSchema('Job listings', 'am', 'job', 'publishTo'),
-              siteSpecificSchema('General page', 'am', 'generalPage', 'publishTo'),
-              // S.documentTypeListItems().filter(
-                //     item => item.getSchemaType().name.startsWith('am-')
-                //   )
-            ])
-        ),
+      .title('All blog posts')
+      .child(
+        S.documentTypeList('post')
+        // .title(``) 
+        .filter('_type == "post"')
+        .child(documentId =>
+          S.document()
+          .documentId(documentId)
+          .schemaType('post')
+        )
+      ),
       S.listItem()
-        .title('Windscope')
-        .child(
-          S.list()
-            .title('Content')
-            .id('ws')
-            .items([
-              siteSpecificSchema('Posts', 'ws', 'post', 'publishTo'),
-              siteSpecificSchema('CTA pages', 'ws', 'ctaPage', 'publishTo'),
-              siteSpecificSchema('General page', 'ws', 'generalPage', 'publishTo'),
-              // S.documentTypeListItems().filter(
-              //     item => item.getSchemaType().name.startsWith('ws-')
-              //   )  
-            ]
-            )
-        ),
-      S.divider(),
-      
-      // The rest of this document is from the original manual grouping in this series of articles
-      ...S.documentTypeListItems().filter(listItem => ![ hiddenFromBase, 'parentStaff', 'category', 'ctaPage', 'job', 'generalPage' ].includes(listItem.getId())),
-    ])
+      .title('Categories')
+      .child(
+        S.documentTypeList('blogPostCategory')
+        .title('Categories')
+        .filter('_type == "blogPostCategory"')
+        .child(documentId =>
+          S.document()
+          .documentId(documentId)
+          .schemaType('blogPostCategory')
+        )
+      ),
+    ])),
+
+    // The rest of this document is from the original manual grouping in this series of articles
+    ...S.documentTypeListItems().filter(listItem => ![hiddenFromBase, 'parentStaff', 'blogPostCategories', 'ctaPage', 'job', 'generalPage', 'post'].includes(listItem.getId())),
+  ])

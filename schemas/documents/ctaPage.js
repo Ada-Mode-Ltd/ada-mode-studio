deefe03ce3b28a29cacc2774e13c41c89605b4f3
Fershad <itsfish@fershad.com>
2022-10-24 17:25:02 +0800

    custom block content for cta page

--- schemas/documents/ctaPage.js
+++ schemas/documents/ctaPage.js
@@ -68,7 +68,42 @@ export default {
           {
             name: 'body',
             title: 'Body',
-            type: 'blockContent',
+            type: 'array',
+      of: [{
+        title: 'Block',
+        type: 'block',
+        // Styles let you set what your user can mark up blocks with. These
+        // correspond with HTML tags, but you can set any title or value
+        // you want and decide how you want to deal with it where you want to
+        // use your content.
+        styles: [
+          {title: 'Normal', value: 'normal'},
+          {title: 'H1', value: 'h1'},
+          {title: 'H2', value: 'h2'},
+          {title: 'H3', value: 'h3'},
+          {title: 'H4', value: 'h4'},
+          {title: 'Quote', value: 'blockquote'},
+        ],
+        lists: [
+          {title: 'Bullet', value: 'bullet'},
+          {title: 'Numbered', value: 'number'}
+        ],
+        // Marks let you mark up inline text in the block editor.
+        marks: {
+          // Decorators usually describe a single property – e.g. a typographic
+          // preference or highlighting by editors.
+          decorators: [
+            {title: 'Strong', value: 'strong'},
+            {title: 'Emphasis', value: 'em'},
+          ]
+        },
+      },
+      {
+        name: 'quote',
+        title: 'Quote',
+        type: 'reference',
+        to: [{ type: 'quote' }],
+      },],
             group: 'content', 
           },
           {

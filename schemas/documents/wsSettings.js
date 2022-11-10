ef037d18a833b7f6fae13f7ecb55b434975df61d
Fershad Irani <27988517+fershad@users.noreply.github.com>
2022-11-10 10:46:38 +0800

    add footer and blog fallback image in settings

--- schemas/documents/wsSettings.js
+++ schemas/documents/wsSettings.js
@@ -21,5 +21,28 @@ export default {
                     }],
 
         },  
+        {
+            name: 'footerLinks',
+            title: 'Footer links',
+            type: 'array',
+            description: 'Set a maximum of 5 menu items',
+            validation: Rule => [
+                Rule.required()
+                    .max(5)
+            ],
+            of: [{ type: 'reference',
+                   to: [{ type: 'page' }],
+                     options: {
+                          filter: 'publishTo == "ws" && !(_id in path("drafts.**"))',
+                     }
+                    }],
+
+        },  
+        {
+            name: 'blogFallbackImage',
+            title: 'Blog post fallback image',
+            type: 'image',
+            description: 'This image will be used as a fallback image for blog posts without a featured image',
+        },  
     ],
 }

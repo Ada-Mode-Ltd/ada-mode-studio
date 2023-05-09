3917981128d2d722e9c5e3f68ccdc861101c7704
fershad <27988517+fershad@users.noreply.github.com>
2023-05-09 12:01:05 +0800

    add netlify dashboard widget

---
+++ dashboardConfig.js
@@ -0,0 +1,26 @@
+export default {
+    widgets: [
+        {
+        name: 'netlify',
+        options: {
+          title: 'Website deploys on Netlify',
+          sites: [
+            {
+              title: 'Windscope',
+              apiId: '6426d52e-aca8-48ff-a674-0d669ff34119',
+              buildHookId: '6372d9488f31233aef25e11c',
+              name: 'verdant-cucurucho-b5ae26',
+              url: 'https://windscope.com'
+            },
+            {
+              title: 'Ada Mode',
+              apiId: 'f63bd42b-238f-46be-9760-27daebcc6c5d',
+              buildHookId: '64351bd18a079d1bf1ee680e',
+              name: 'velvety-salmiakki-87c150',
+              url: 'https://velvety-salmiakki-87c150.netlify.app',
+            }
+          ]
+        }
+      }
+    ]
+  }
